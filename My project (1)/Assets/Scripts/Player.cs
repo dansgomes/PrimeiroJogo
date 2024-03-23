e200eaa252a79577b5558eb0ea04d9b01a98a8f5
Daniel de Souza Gomes <103148478+dansgomes@users.noreply.github.com>
2024-03-22 21:11:00 -0300

    Variáveis e Métodos
    Criados as variáveis e métodos dos personagens a fim de testes.

---
+++ My project (1)/Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public int vidaPlayer = 10;
+    public float velocidade = 1.2f;
+    bool estaVivo = false;
+    string nomePlayer = "Tião";
+
+    public GameObject chao;
+    public Transform inimigo;
+
+    void Attack()
+    {
+
+    }
+
+    void Movement()
+    {
+
+    }
+
+    void Jump()
+    {
+
+    }
+}
