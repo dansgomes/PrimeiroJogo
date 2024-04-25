2a4ec003f43d0f107dd94fa5766833dd0979cf95
Daniel de Souza Gomes <103148478+dansgomes@users.noreply.github.com>
2024-04-24 21:41:57 -0300

    Npc dialogs added
    Added dialogs for the idle Npc in the map.
    
    Adicionados diálogos para o Npc idle no mapa.

---
+++ My project (1)/Assets/Scripts/Dialog/DialogSettings.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Dialog", menuName = "New Dialog/Dialog")]
+public class NewBehaviourScript : ScriptableObject
+{
+    [Header("Settings")]
+    public GameObject actor;
+
+    [Header("Dialog")]
+    public Sprite speakerSprite;
+    public string sentence;
+
+    public List<Sentences> dialogs = new List<Sentences>();
+}
+
+[System.Serializable]
+public class Sentences
+{
+    public string actorName;
+    public Sprite profile;
+    public Languages sentence;
+}
+
+[System.Serializable]
+public class Languages
+{
+    public string portuguese;
+    public string english;
+    public string spanish;
+}
