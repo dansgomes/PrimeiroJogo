416071b9d5d7d67a33e13ddc1a1070820430949d
Daniel de Souza Gomes <103148478+dansgomes@users.noreply.github.com>
2024-06-25 21:49:03 -0300

    Enemy attack radius added
    Added radius, colission and logic for the enemy to confirm the hit in the player.
    
    Adicionado alcance, colisão e lógica para que o inimigo confirme o acerto no jogador.

--- My project (1)/Assets/Scripts/Enemy/AnimationControl.cs
+++ My project (1)/Assets/Scripts/Enemy/AnimationControl.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class AnimationControl : MonoBehaviour
 {
+    [SerializeField] private Transform attackPoint;
+    [SerializeField] private float radius;
+    [SerializeField] private LayerMask playerLayer;
+
+
     private Animator anim;
 
     private void Start()
@@ -14,5 +19,25 @@ public class AnimationControl : MonoBehaviour
     public void PlayAnim(int value)
     {
         anim.SetInteger("transition", value);
+    }
+
+    public void Attack()
+    {
+        Collider2D hit = Physics2D.OverlapCircle(attackPoint.position, radius, playerLayer);
+
+        if (hit != null)
+        {
+            //detecta colisão com player
+            Debug.Log("bateu no player");
+        }
+        else
+        {
+
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(attackPoint.position, radius);
     }
 }
