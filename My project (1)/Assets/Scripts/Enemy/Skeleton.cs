2835d8dad33a9b3228f429ff3870659d4e15eaa8
Daniel de Souza Gomes <103148478+dansgomes@users.noreply.github.com>
2024-07-01 23:35:28 -0300

    Enemy health and death added
    Added a health bar for the skeleton that will go down after receiving damage; when turning into zero the enemy will die and disappear.
    
    Adicionada uma barra de vida para o esqueleto que irá descer depois de receber dano; quando torna-se zero o inimigo irá morrer e desaparecer.

--- My project (1)/Assets/Scripts/Enemy/Skeleton.cs
+++ My project (1)/Assets/Scripts/Enemy/Skeleton.cs
@@ -2,10 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class Skeleton : MonoBehaviour
 {
+    [Header("Stats")]
+    public float totalHealth;
+    public float currentHealth;
+    public Image healthBar;
+    public bool isDead;
+
     //[SerializeField] private float speed;
+    [Header("Components")]
     [SerializeField] private NavMeshAgent agent;
     [SerializeField] private AnimationControl animControl;
 
@@ -14,6 +22,7 @@ public class Skeleton : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        currentHealth = totalHealth;
         player = FindObjectOfType<Player>();
         agent.updateRotation = false;
         agent.updateUpAxis = false;
@@ -22,27 +31,31 @@ public class Skeleton : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        agent.SetDestination(player.transform.position);
+        if (!isDead)
+        {
+            agent.SetDestination(player.transform.position);
 
-        if(Vector2.Distance(transform.position, player.transform.position) <= agent.stoppingDistance)
-        {
-            //chegou no limite de distancia / skeleton para
-            animControl.PlayAnim(2);
-        }
-        else
-        {
-            //skeleton segue o player
-            animControl.PlayAnim(1);
-        }
+            if (Vector2.Distance(transform.position, player.transform.position) <= agent.stoppingDistance)
+            {
+                //chegou no limite de distancia / skeleton para
+                animControl.PlayAnim(2);
+            }
+            else
+            {
+                //skeleton segue o player
+                animControl.PlayAnim(1);
+            }
 
-        float posX = player.transform.position.x - transform.position.x;
-        if(posX > 0)
-        {
-            transform.eulerAngles = new Vector2(0, 0);
+            float posX = player.transform.position.x - transform.position.x;
+            if (posX > 0)
+            {
+                transform.eulerAngles = new Vector2(0, 0);
+            }
+            else
+            {
+                transform.eulerAngles = new Vector2(0, 180);
+            }
+
         }
-        else
-        {
-            transform.eulerAngles = new Vector2(0, 100);
-        }
-    }
+    }   
 }
